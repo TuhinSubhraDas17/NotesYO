d5f0cfd1140b8885111d2c1e8f786ca8c9745a71
Tuhin Subhra Das <54277974+TuhinSubhraDas17@users.noreply.github.com>
2021-07-18 21:56:37 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BookmarkIcon from '@material-ui/icons/Bookmark';
+
+function Header() {
+  return (
+    <header>
+      <h1><BookmarkIcon />NotesYo</h1>
+    </header>
+  );
+}
+
+export default Header;
